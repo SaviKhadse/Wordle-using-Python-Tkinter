be7a557c982a39fa3adc17149aee8423680a1b23
SaviKhadse <savitakhadse@gmail.com>
2023-01-20 16:10:51 -0800

    version5

--- tkinter_dbconnection_file.py
+++ tkinter_dbconnection_file.py
@@ -3,43 +3,42 @@ from sqlite3 import Cursor
 
 
 class mysqlCon:
-    conn = ""
-
+    conn=""
     def __init__(self):
         print("Hello, this is class mysqlCon from file testdbfile...")
+
 
     def fndbconn():
         global conn
         import mysql.connector as mysqlConnector
-        conn = mysqlConnector.connect(
-            host='localhost', user='root', password='rootroot1234', database='PythonWordle')
+        conn = mysqlConnector.connect(host='localhost',user='root',password='sagar@123',database='testdb')
         if conn:
             return 1
         else:
             return 0
 
-    def fnLogin(tmpuser, tmppass):
+    def fnLogin(tmpuser,tmppass):
         global conn
-        mycursor = conn.cursor()
-
+        mycursor=conn.cursor()
+        
         print(f"Entered Username = {tmpuser}")
         print(f"Entered Password = {tmppass}")
 
-        tuple1 = (tmpuser, tmppass)
+        tuple1 = (tmpuser,tmppass)
 
-        query = """select * from users1 where username=%s and password=%s"""
+        query="""select * from users where username=%s and password=%s"""
 
-        result = mycursor.execute(query, tuple1)
-        result = mycursor.fetchall()
+        result=mycursor.execute(query,tuple1)
+        result=mycursor.fetchall()
         print(f"Result = {result}")
 
         return result
-        # conn.close()
-
-    def fnRegister(username, password, tmpemailid):
-        mycursor = conn.cursor()
+        
+        conn.close()
+    
+    def fnRegister(username,password,tmpemailid):
+        mycursor=conn.cursor() 
         print(username, password, tmpemailid)
-        mycursor.execute(
-            f"insert into users1(username,password,email_id) values('{username}','{password}','{tmpemailid}')")
+        mycursor.execute(f"insert into users(username,password,email_id) values('{username}','{password}','{tmpemailid}')")
         conn.commit()
         conn.close()
--- tkinter_wordle_game_main_loagical_program.py
+++ tkinter_wordle_game_main_loagical_program.py
@@ -1,62 +1,73 @@
 from argparse import _CountAction
+from operator import length_hint
 import random
 
 global tmpAnswer
-global length
-global tmpChoice
+global tmpRandomChoice
 global listAns
 global countC
 global countI
 global listUserAns
 global finalWord
-chanceCount = 1
-
+chanceCount=1
 
 class WordleClass:
     def __init__(self):
         print("Hello, this is class mysqlCon from file testdbfile...")
 
-    # This code is to get final word list from file.
-    listAns = []
-    file = open(
-        "/Users/savitakhadse/Desktop/Spring/Python/PythonFinal project/wordle.txt", "r")
-    str = file.read().split('\n')
 
-    listAns = str
-    file.close()
-    print(listAns)
-    # End of the code.
+    def getNewFinalWord():
+        global listAns
+        global tmpRandomChoice
+        global listFinal
+        #This code is to get final word list from file.
+        listAns=[]
+        file=open("C:\\Users\ma29h\Downloads\wordle.txt","r")
+        str=file.read().split('\n')
 
-    # Generating random number's word for today's game.
-    tmpRandomChoice = random.randrange(0, 5)
+        listAns=str
+        file.close()
+        print(listAns)
+        #End of the code.
 
-    # Random generated word length is here.
+        #Generating random number's word for today's game.
+        tmpRandomChoice=random.randrange(0,4)
 
-    # This to store random generated word for Game.
-    i = 1
-    finalWord = listAns[tmpRandomChoice]
-    global listFinal
-    listFinal = []
-    listFinal.clear()
-    for i in range(5):
-        listFinal.append(finalWord[i])
-        print(listFinal)
+        #This to store random generated word for Game.
+        i=1
+        global finalWord
+        finalWord=listAns[tmpRandomChoice]
+        global listFinal
+        listFinal=[]
+        listFinal.clear()
+        for i in range(4):
+            listFinal.append(finalWord[i])
+            print(listFinal)
+        return finalWord
+
+    def getCorrectAnswer():
+        global finalWord
+        print(f"Correct Answer = {finalWord}")
+        return finalWord
+
+
 
     def fnStoreUserInputList(tmpAnswer):
-        # Here, starting the storing user input in list from here.
+        #Here, starting the storing user input in list from here.
+        global listFinal
         global listUserAns
-        listUserAns = []
+        listUserAns=[]
         listUserAns.clear()
-        j = 1
-        for j in range(5):
+        j=1
+        for j in range(4):
             listUserAns.append(tmpAnswer[j])
             print(listUserAns)
 
-        # Matching User and Final Answer.
+        #Matching User and Final Answer.
         global countC
         global countI
 
-        # This list to be used to make TKINTER UI , green-yellow-grey as per the answer.
+        #This list to be used to make TKINTER UI , green-yellow-grey as per the answer.
         global greenList
         global yellowList
         global greyList
@@ -64,61 +75,62 @@ class WordleClass:
         greenList = []
         yellowList = []
         greyList = []
-        # -------------End ------------.
+        #-------------End ------------.
 
-        countC = 0
-        countI = 0
-        k = 0
-        l = 0
+        countC=0
+        countI=0
+        k=0
+        l=0
 
         #
-        for k in range(5):
+        for k in range(4):
             print(f"\nChecking result for the #{k+1} letter.")
-            flag1 = 0
-            flag2 = 0
-            flag3 = 0
-            for l in range(5):
-                user = listUserAns[k]
-                computer = listFinal[l]
+            flag1=0
+            flag2=0
+            flag3=0
+            for l in range(4):
+                user=listUserAns[k]
+                computer=listFinal[l]
 
                 #listUserAns = A,B,C,D
                 #listFinal = B,F,C,H
-                # countC=1
-                # countI=3
+                #countC=1
+                #countI=3
                 #greyList = [1,4]
                 #yellowList = [2]
                 #greenList = [3]
 
-                # user==compter is used for letter matching, k==l is used for position matching.
-                if user == computer and k == l:
-                    flag1 = 1
-                elif user == computer and k != l:
-                    flag2 = 2
-                elif user != computer:
-                    flag3 = 3
+                #user==compter is used for letter matching, k==l is used for position matching.
+                if user==computer and k==l:
+                    flag1=1
+                elif user==computer and k!=l:
+                    flag2=2
+                elif user!=computer:
+                    flag3=3
                 else:
                     print("")
 
-            if flag1 == 1 and flag3 == 3:
-                # print(f"Position of letter #{user} is correct.")
+            if flag1==1 and flag3==3:
+                #print(f"Position of letter #{user} is correct.")
                 print(f"\033[1;32;40m {user}  \n")
-                countC += 1
+                countC+=1
                 greenList.append(k+1)
-            elif flag2 == 2 and flag3 == 3:
-                # print(f"This #{user} letter is exist but position is not correct.")
+            elif flag2==2 and flag3==3:             
+                #print(f"This #{user} letter is exist but position is not correct.")
                 print(f"\033[1;33;40m {user}  \n")
-                countI += 1
+                countI+=1
                 yellowList.append(k+1)
-            elif flag3 == 3:
-                # print(f"This #{user} letter is not exist.")
+            elif flag3==3:
+                #print(f"This #{user} letter is not exist.")
                 print(f"\033[1;37;40m {user}  \n")
-                countI += 1
+                countI+=1
                 greyList.append(k+1)
             else:
                 print("")
-
-        # Calling function to check final answer and user limits.
+        
+        #Calling function to check final answer and user limits.
         return greenList, yellowList, greyList
 
-        # Function to check maximum limit to get answer from user if user is wrong.
-        # Function calling for user input and it's length validation.
+
+        #Function to check maximum limit to get answer from user if user is wrong.
+        #Function calling for user input and it's length validation.
